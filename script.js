9505487c1b0f672eaeae9b6d7c6ed31a079883b7
@ekramdev <karoom282030@gmail.com>
2025-08-10 07:59:36 +0300

    create commits & use math random method to convert it to strings

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-
-function getComputerChoice(str){
-    return 
+humanScore = 0 ;
+computerScore = 0 ;
+function getComputerChoice(){
+    let mathRandom = Math.floor(Math.random()*3);
+    if (mathRandom == 0) {
+       return "rock";
+    }else if (mathRandom == 1) {
+        return "paper";
+    }
+    return "sissors";
 }
+function getHumanChoice(){
+    let choice = prompt("please choose a  random number ");
+    let mathRandom;
+    if(choice == 0){
+        return "rock";
+    }else if (choice == 1 ){
+        return "paper;"
+    }else {
+        return "sissors"
+    }
+}
+console.log(getHumanChoice());
+console.log(getHumanChoice());
+console.log(getHumanChoice());
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
 console.log(getComputerChoice());
